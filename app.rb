7d4008b2926343543968282eea60d6e8b0f04c63
Ajay <ajaymungur@hotmail.com>
2018-11-12 16:50:31 +0000

    Added surname instance variable and if, elsif statement so any combination can be added

--- app.rb
+++ app.rb
@@ -16,7 +16,13 @@ get '/page2' do
   "Wilcommen"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name =  ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p @name = params[:name]
+  @surname = params[:surname]
   erb(:index)
 end
